3f29d87cbaa439d9f2ed15109dca7e0726f005e1
Richard979-dev <78233934+Richard979-dev@users.noreply.github.com>
2021-01-29 23:57:45 +0200

    Add files via upload

---
+++ arithmetic mean/arithmetic mean/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace arithmetic_mean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Input number and stop when ready");
+
+            List<double> numbers = new List<double>();
+
+
+            while (true)
+            {
+                string str = Console.ReadLine();
+                if (str.Equals("stop"))
+                {
+                    Console.WriteLine("Average = " + numbers.Average());
+                    Console.ReadLine();
+
+                    return;
+                }
+                else
+                {
+                    try {
+                        double number = double.Parse(str);
+                        numbers.Add(number);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("BOLVAN !!!!");
+                    }
+                    
+                }
+            }
+            
+        
+        }
+    }
+}
